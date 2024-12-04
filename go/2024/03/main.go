bc9e73f6b31677e71fe4874426b778a4d06837c1
Jared Griggs <griggsjared@gmail.com>
2024-12-03 17:59:28 -0700

    add day 3/2004 for golang

--- go/2024/03/main.go
+++ go/2024/03/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
 	"github.com/griggsjared/advent-of-code/go/internal"
 )
 
@@ -13,10 +18,75 @@ func main() {
 	internal.ProcessAndDisplayResults(day, year, Part1, Part2)
 }
 
-func Part1(i string) (int, error) {
-	return 0, nil
+func Part1(input string) (int, error) {
+	rows := strings.Split(input, "\n")
+	return sumMulRows(rows), nil
 }
 
-func Part2(i string) (int, error) {
-	return 0, nil
+func Part2(input string) (int, error) {
+	//input with be lines of "corrupted" text strings,
+	//this time there will be do() and don't() that will act like on off switches to prcess the proceeding mul(#,#) substrings,
+	//the start of the string always starts with do(), all of mul() up to the don't() are active and will be processed, any mul() after the don't() will not be processed up til the next do()
+	inDo := true //start is active
+  row := ""
+	for i := range input {
+		if strings.HasPrefix(input[i:], "don't()") {
+			inDo = false
+		} else if strings.HasPrefix(input[i:], "do()") {
+			inDo = true
+		}
+		if inDo {
+			row += string(input[i])
+		}
+	}
+  rows := make([]string, 0)
+  rows = append(rows, row)
+
+	return sumMulRows(rows), nil
+}
+
+func sumMulRows(rows []string) int {
+	sum := 0
+	for _, row := range rows {
+		muls := extractMuls(row)
+		for _, mul := range muls {
+			mulVal, err := processMul(mul)
+			if err != nil {
+				panic(err)
+			}
+			sum += mulVal
+		}
+	}
+	return sum
+}
+
+func extractMuls(s string) []string {
+	//we can use regex to find the mul(#,#) substrings
+	//we will return a slice of strings that contain the mul(#,#) substrings
+	r, _ := regexp.Compile("mul\\(\\d+,\\d+\\)")
+
+	muls := make([]string, 0)
+	muls = r.FindAllString(s, -1)
+	//find all the mul(#,#) substrings
+	return muls
+}
+
+func processMul(mul string) (int, error) {
+
+	//make sure the mul string is in the correct format
+	//example: mul(2,4)
+	r, _ := regexp.Compile("mul\\(\\d+,\\d+\\)")
+	if !r.MatchString(mul) {
+		return 0, fmt.Errorf("Invalid mul string: %s", mul)
+	}
+
+	//extract the digits out of the mul string
+	r2, _ := regexp.Compile("\\d+")
+	digits := r2.FindAllString(mul, -1)
+
+	//multiply the digits together
+	num1, _ := strconv.Atoi(digits[0])
+	num2, _ := strconv.Atoi(digits[1])
+
+	return num1 * num2, nil
 }
--- go/2024/03/main_test.go
+++ go/2024/03/main_test.go
@@ -6,9 +6,11 @@ import (
 	main "github.com/griggsjared/advent-of-code/go/2024/03"
 )
 
-const input string = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
 func TestPart1(t *testing.T) {
+
+  input := `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+
 	count, err := main.Part1(input)
 	if err != nil {
 		t.Error(err)
@@ -20,13 +22,16 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
+
+  input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
+
 	count, err := main.Part2(input)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if count != 0 {
-		t.Errorf("Expected 0, got %d", count)
+	if count != 48 {
+		t.Errorf("Expected 48, got %d", count)
 	}
 }
 
