206b5c779b87a38c6a36bca1f8bd0cf0aa00b7e3
Jared Griggs <griggsjared@gmail.com>
2024-12-06 08:37:24 -0700

    add some extra formatting and time outputs to results

--- go/internal/helper.go
+++ go/internal/helper.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func LoadDayInput(day int, year int) (string, error) {
-  fpath := fmt.Sprintf("./%d/%02d/input.txt", year, day)
+	fpath := fmt.Sprintf("./%d/%02d/input.txt", year, day)
 
-  i, err := os.ReadFile(fpath)
+	i, err := os.ReadFile(fpath)
 	if err != nil {
 		return "", err
 	}
@@ -29,31 +30,31 @@ func LoadDayInput(day int, year int) (string, error) {
 type FmtColor int
 
 const (
-  Black FmtColor = 30
-  Red FmtColor = 31
-  Green FmtColor = 32
-  Yellow FmtColor = 33
-  Blue FmtColor = 34
-  Magenta FmtColor = 35
-  Cyan FmtColor = 36
-  White FmtColor = 37
+	Black   FmtColor = 30
+	Red     FmtColor = 31
+	Green   FmtColor = 32
+	Yellow  FmtColor = 33
+	Blue    FmtColor = 34
+	Magenta FmtColor = 35
+	Cyan    FmtColor = 36
+	White   FmtColor = 37
 )
 
 func (c FmtColor) Validate() bool {
-  switch c {
-  case Black, Red, Green, Yellow, Blue, Magenta, Cyan, White:
-    return true
-  default:
-    return false
-  }
+	switch c {
+	case Black, Red, Green, Yellow, Blue, Magenta, Cyan, White:
+		return true
+	default:
+		return false
+	}
 }
 
 func FmtWithColor(color FmtColor, format string, args ...any) string {
-  if !color.Validate() {
-    panic("Invalid color")
-  }
-  
-  return fmt.Sprintf("\x1b[%dm%s\x1b[0m", int(color), fmt.Sprintf(format, args...))
+	if !color.Validate() {
+		panic("Invalid color")
+	}
+
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", int(color), fmt.Sprintf(format, args...))
 }
 
 // partFunc is a function that takes a string and returns an int and an error,
@@ -61,28 +62,34 @@ func FmtWithColor(color FmtColor, format string, args ...any) string {
 type partFunc func(i string) (int, error)
 
 func ProcessAndDisplayResults(day int, year int, p1 partFunc, p2 partFunc) {
-  fmt.Println(FmtWithColor(Blue, "Advent of Code %d - Day %d", year, day))
+	fmt.Println(FmtWithColor(Blue, "Advent of Code %d - Day %d", year, day))
 
-  input, err := LoadDayInput(day, year)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	input, err := LoadDayInput(day, year)
+	if err != nil {
+		fmt.Println(FmtWithColor(Red, err.Error()))
+		return
+	}
 
-  fmt.Println(FmtWithColor(Green, "Part 1:"))
-  result1, err := p1(input)
-  if err != nil {
-    fmt.Println(FmtWithColor(Red, err.Error()))
-    return
-  }
-  fmt.Println(FmtWithColor(Cyan, fmt.Sprintf("%d 🌟", result1)))
+	sTime := time.Now()
 
-  fmt.Println(FmtWithColor(Green, "Part 2:"))
-  result2, err := p2(input)
-  if err != nil {
-    fmt.Println(FmtWithColor(Red, err.Error()))
-    return
+	fmt.Println(FmtWithColor(Green, "Part 1:"))
+	result1, err := p1(input)
+	if err != nil {
+		fmt.Println(FmtWithColor(Red, err.Error()))
+		return
   }
-  fmt.Println(FmtWithColor(Cyan, fmt.Sprintf("%d 🌟", result2)))
+	eTime := time.Now()
+	fmt.Println(FmtWithColor(Cyan, fmt.Sprintf("%d 🌟", result1)))
+	fmt.Println(FmtWithColor(Yellow, "(Time: %v)", eTime.Sub(sTime)))
+
+	sTime = time.Now()
+	fmt.Println(FmtWithColor(Green, "Part 2:"))
+	result2, err := p2(input)
+	if err != nil {
+		fmt.Println(FmtWithColor(Red, err.Error()))
+		return
+	}
+	eTime = time.Now()
+	fmt.Println(FmtWithColor(Cyan, fmt.Sprintf("%d 🌟", result2)))
+	fmt.Println(FmtWithColor(Yellow, "(Time: %v)", eTime.Sub(sTime)))
 }
-
