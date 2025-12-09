508d60de0b83336f65c9fa9bbf8d5d8da17b5f6d
Jared Griggs <griggsjared@gmail.com>
2025-12-09 07:39:44 -0700

    refactor(go): update starter test with expected value

--- go/starter/main_test.go
+++ go/starter/main_test.go
@@ -10,26 +10,28 @@ const input string = ``
 
 func TestPart1(t *testing.T) {
 
+	expected := 0;
 	count, err := main.Part1(input)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	if count != 0 {
-		t.Errorf("Expected 0, got %d", count)
+	if count != expected {
+		t.Errorf("Expected %d, got %d", expected, count)
 	}
 }
 
 func TestPart2(t *testing.T) {
 
+	expected := 0;
 	count, err := main.Part2(input)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	if count != 0 {
-		t.Errorf("Expected 0, got %d", count)
+	if count != expected {
+		t.Errorf("Expected %d, got %d", expected, count)
 	}
 }
